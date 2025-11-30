56fc63d6208c40c91e5f2b06fbb2a757ec0f110e
immehedi49 <m.hasanrafin49@gmail.com>
2025-11-30 18:16:18 +0600

    logo image

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <img src="../images/global.jpg" alt="globe icon" />
+            <img src="../images/global.jpg" alt="" />
             <h1>my travel journal.</h1>
         </header>
     )
